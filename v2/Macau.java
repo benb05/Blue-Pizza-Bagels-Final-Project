f179d0bd3fa7d54756ad5aca22a7fae65431f8b4
kostadubovskiy <kostadubovskiy@gmail.com>
2022-01-19 21:57:55 -0500

    macau draft

--- v2/Macau.java
+++ v2/Macau.java
@@ -3,10 +3,15 @@ import java.util.*;
 public class Macau {
     private ArrayList<Hand> hands = new ArrayList<Hand>(2); 
     private Deck _deck = new Deck(); 
+
     public Macau() {
-        Hand compHand = new Hand(); 
-        for(int i = 0; i < 6; i++) {
+        Hand compHand = new Hand(); // computer hand
+        Hand p1Hand = new Hand(); // player 1 hand
+        hands.set(0, compHand);
+        hands.set(1, p1Hand);
+        for(int i = 0; i < 6; i++) { // populate hands
             compHand.set(i, _deck.draw());
+            p1Hand.set(i, _deck.draw());
         }
     }
 
