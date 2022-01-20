2922963508c9a46772a79b4f89dd7e31923b267f
benb05 <bbelotser30@stuy.edu>
2022-01-20 13:21:38 -0500

    hi

---
+++ v2/Woo.java
@@ -0,0 +1,16 @@
+import java.line-breaker;
+
+public class Woo{
+
+
+
+
+
+
+
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Macau, brought to you by Blue Pizza Bagels Inc.")
+    System.out.println("Rules: Players are given six cards from a standard 52 card deck. The first player to go can put down any card.  After the first turn, the next player can put down a card that is either the same suit or number as the one put down the turn before. There are four card numbers with special functions. placing a 2 ")
+  }
+}
