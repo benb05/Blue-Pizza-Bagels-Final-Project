d017f9f1778578e76f2e16b5f95bb9359b56b368
kostadubovskiy <kostadubovskiy@gmail.com>
2022-01-19 22:07:40 -0500

    macau reconsideration

--- v2/Macau.java
+++ v2/Macau.java
@@ -4,18 +4,22 @@ public class Macau {
     private ArrayList<Hand> hands = new ArrayList<Hand>(2); 
     private Deck _deck = new Deck(); 
 
-    public Macau() {
+    public Macau(String[] args) {
         Hand compHand = new Hand(); // computer hand
         Hand p1Hand = new Hand(); // player 1 hand
-        hands.set(0, compHand);
-        hands.set(1, p1Hand);
+        hands.set(0, p1Hand);
+        hands.set(1, compHand);
         for(int i = 0; i < 6; i++) { // populate hands
             compHand.set(i, _deck.draw());
             p1Hand.set(i, _deck.draw());
         }
     }
 
-    public Macau(int numPlayers) {
+    /* public Macau(int numPlayers) {
+
+    } */
+
+    public Hand getHand(int ind) {
 
     }
 }
