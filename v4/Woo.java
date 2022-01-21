7ebff424dd1b4326b1ab40789ae7a8d691d42d36
benb05 <bbelotser30@stuy.edu>
2022-01-21 12:51:00 -0500

    hi

--- v4/Woo.java
+++ v4/Woo.java
@@ -2,10 +2,13 @@ public class Woo{
 
 
   final static private String intro = "Welcome to Macau, brought to you by Blue Pizza Bagels Inc.\n"+
+  "Board Members: Andrew Piatetsky, Kosta Dubovskiy, Benjamin Belotser \n" +
   "Rules:\n" + 
   "Players are given six cards from a standard 52 card deck. The first player to go can put down any card.\n" + 
   "After the first turn, the next player can put down a card that is either the same suit or number as the previous card played.\n" + 
-  "There are four card numbers with special functions. \n" + ""
+  "There are four card numbers with special functions. \n" + "The 2 card forces the other player to draw 2 cards and skip their turn. \n" +
+  "The 4 card functions the same as two, except it forces the player to draw four instead of two cards. \n" + 
+  ""
   
   
   ;
@@ -15,8 +18,7 @@ public class Woo{
 
 
   public static void main(String[] args) {
-    // System.out.println("Welcome to Macau, brought to you by Blue Pizza Bagels Inc.")
-    // System.out.println("Rules: Players are given six cards from a standard 52 card deck. The first player to go can put down any card.  After the first turn, the next player can put down a card that is either the same suit or number as the one put down the turn before. There are four card numbers with special functions. placing a 2 ")
+
     System.out.println(intro);
   }
 }
