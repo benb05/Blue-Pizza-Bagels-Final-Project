8a41cdec5987c000e02d00a7a854d1e1035f454f
benb05 <benjaminbelotser@gmail.com>
2022-01-21 12:03:24 -0500

    uiedits

--- v3/Woo.java
+++ v3/Woo.java
@@ -4,8 +4,8 @@ public class Woo{
   final static private String intro = "Welcome to Macau, brought to you by Blue Pizza Bagels Inc.\n"+
   "Rules:\n" + 
   "Players are given six cards from a standard 52 card deck. The first player to go can put down any card.\n" + 
-  "After the first turn, the next player can put down a card that is either the same suit or number as the one put down the turn before.\n" + 
-  "There are four card numbers with special functions. placing a 2\n"
+  "After the first turn, the next player can put down a card that is either the same suit or number as the previous card played.\n" + 
+  "There are four card numbers with special functions. \n" + ""
   
   
   ;
