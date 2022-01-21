083c7b4af12909680443fadd9bf932b356c9415c
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-01-21 13:58:23 -0500

    more updates to macau

--- v4/Macau.java
+++ v4/Macau.java
@@ -21,6 +21,12 @@ public class Macau {
         }
     }
 
+    /**
+     * accessor method: allows program to access a specific hand. 
+     * Throws error if index is out of bounds
+     * @param index
+     * @return hand
+     */
     public Hand getHand(int index) {
         return _hands.get(index);
     }
@@ -29,21 +35,21 @@ public class Macau {
      * Checks if any players have empty _hands—aka if anyone has won 
      * Returns that player's hand index in the array _hands if anyone has won
      * Returns -1 if no one has won 
-     * @return -1 or index
+     * @return -1 or index(0-1)
      */
     public int gameOver() {
         Hand hand = new Hand();
-        for(int i = 0; i < _hands.size(); i++) {
-            hand = _hands.get(i);
-            if(hand.isEmpty()) {
-                return i;
+        for(int i = 0; i < _hands.size(); i++) { // cycles through hands arr list 
+            hand = _hands.get(i); // temp var of current index of hands arr list (a hand object)
+            if(hand.isEmpty()) { // checks to see if hand is empty
+                return i; // if its empty returns that index of hands arr list
             }
         }
-        return -1;
+        return -1; // if none of the hands are empty then returns -1, game continues
     }
 
     /**
-     * checks to see if there is at least on playable card in a specified hand
+     * checks to see if there is at least one playable card in a specified hand
      * @param h
      * @return boolean
      */
@@ -58,17 +64,17 @@ public class Macau {
         return false;
     }
 
-
+    /**
+     * takes index of chosen card for the active player's hand
+     * if it's playable, play it(remove from their hand, add to top of discard)
+     * return 1 to indicate success
+     * if not, return 0 to indicate failure
+     * @return 0 or 1
+     */
     public int runTurn() {
-        /* 
-            * takes index of chosen card for the active player's hand
-            * if it's playable, play it(remove from their hand, add to top of discard)
-                * return 1 to indicate success
-            * if not, return 0 to indicate failure
-        */
-        Hand currHand = _hands.get(turnNumber % 2);
+        Hand currHand = _hands.get(_turnNumber % 2);
         Scanner sc = new Scanner(System.in);  // Create a Scanner object
-        String retString = "Please input which card you'd like to play(0-" + (_hands.get(1).size() - 1) + ")";
+        String retString = "Please input which card you'd like to play(0-" + (_hands.get(1)) + ")";
         String cardNum = sc.nextLine();  // Read user input
         if(_deck.playable(currHand.get(cardNum))) { // checks if the card is playable
             _deck.putInDiscard(currHand.play(cardNum)); // play the card chosen
