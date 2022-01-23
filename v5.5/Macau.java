e183bacabbbf0e6e88106a48fa98d6910054d613
kostadubovskiy <kostadubovskiy@gmail.com>
2022-01-23 13:50:27 -0500

    merge errors version

---
+++ v5.5/Macau.java
@@ -0,0 +1,244 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Macau {
+    
+    private Deck _deck; 
+    private ArrayList<Hand> _hands = new ArrayList<Hand>(); 
+    private int _turnNumber = 1; // the number of the turn, for 1 player 1 computer, odd turns are player turns evens are computer turns
+
+    public Macau() {
+        _deck = new Deck();
+        Hand compHand = new Hand(); // computer hand
+        Hand p1Hand = new Hand(); // player 1 hand
+
+        _hands.add(compHand);
+        _hands.add(p1Hand);
+
+        for(int i = 0; i < 6; i++) { // populate _hands
+            compHand.add(_deck.draw(1));
+            p1Hand.add(_deck.draw(2));
+        }
+    }
+
+    /**
+     * accessor method: allows program to access a specific hand. 
+     * Throws error if index is out of bounds
+     * @param index
+     * @return hand
+     */
+    public Hand getHand(int index) {
+        return _hands.get(index);
+    }
+
+    /**
+     * Checks if any players have empty _hands—aka if anyone has won 
+     * Returns that player's hand index in the array _hands if anyone has won
+     * Returns -1 if no one has won 
+     * @return -1 or index(0-1)
+     */
+    public int gameOver() {
+        Hand hand = new Hand();
+        for(int i = 0; i < _hands.size(); i++) { // cycles through hands arr list 
+            hand = _hands.get(i); // temp var of current index of hands arr list (a hand object)
+            if(hand.isEmpty()) { // checks to see if hand is empty
+                return i; // if its empty returns that index of hands arr list
+            }
+        }
+        return -1; // if none of the hands are empty then returns -1, game continues
+    }
+
+    /**
+     * checks to see if there is at least one playable card in a specified hand
+     * @param h
+     * @return boolean
+     */
+    public boolean hasPlayableCard(Hand h) {
+        Card c = new Card();
+        for (int i = 0; i < h.size(); i++) {
+            c = h.getCard(i);
+            if (_deck.playable(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // public int runTurn() {
+    //     Hand currHand = _hands.get(_turnNumber % 2);
+    //     Scanner sc = new Scanner(System.in);  // Create a Scanner object
+    //     System.out.println("Here is your current hand: ");
+    //     System.out.println(currHand);
+    //     System.out.println("Here is the top card in the deck: ");
+    //     System.out.println(_deck.getLastCard());
+    //     String retString = "Please input which card you'd like to play by it's index(0-" + (currHand.size() - 1) + ")";
+    //     System.out.println(retString);        
+    //     Integer cardNum = Integer.parseInt(sc.nextLine());  // Read user input
+    //     System.out.println("");
+    //     if(_deck.playable(currHand.getCard(cardNum))) { // checks if the card is playable
+    //         System.out.println(currHand.play(cardNum));
+    //         _deck.putInDiscard(currHand.play(cardNum)); // play the card chosen
+    //         _deck.updateLastCard(currHand.play(cardNum));
+    //         return 1;
+    //     } else {
+    //         return -1; // indicate that the card cannot be played
+    //     }
+    // }
+
+    /**
+     * Simulates a turn in macau
+     * takes input of chosen card for the active player's hand
+     * if it's playable, play it (remove from their hand, add to top of discard)
+     * return 1 to indicate success
+     * if not, return -1 to indicate failure
+     * return 0 if there are no cards that are playable in hand (take card and skip turn)
+     * returns 2 if card chosen has a special function
+     * @return -1,0,1,2
+     */
+    public int runTurn() {
+        Card c = new Card();
+
+        int whoseTurn = _turnNumber % 2; // 0 (when even, computer) or 1 (when odds, player)
+        Hand currHand = _hands.get(whoseTurn); 
+
+        // no playable card conditions - skip turn and draw
+        if (!hasPlayableCard(currHand)) {
+            if (whoseTurn == 1) {
+                System.out.println("You don't have any cards to play, you skip a turn and take a card");
+                c = _deck.draw(2);
+                currHand.add(c);
+                return 0;
+            }
+            if (whoseTurn == 0) {
+                System.out.println("Opponent doesn't have any cards to play, they skip their turn and draw a card");
+                c = _deck.draw(1);
+                currHand.add(c);
+                return 0;
+            }
+        }
+
+        // if there is a playable card in hand
+        int cardIndex = -1;
+        if (whoseTurn == 1) {
+            cardIndex = userInput(currHand);
+        }
+        if (whoseTurn == 0) { // computer simple ai
+            for (int i = 0; i < currHand.size(); i++) {
+                c = currHand.getCard(i);
+                if (_deck.playable(c)) {
+                    cardIndex = i;
+                }
+            }
+        }
+
+        c = currHand.getCard(cardIndex); //-1 because the first card is 1 and the last card is n+1
+
+        if(_deck.playable(c)) { // double checks if the card is playable
+            c.changeWhere(-1); // marks it to be placed in discard
+            _deck.putInDiscard(currHand.play(cardIndex)); // play the card chosen and place it in discard
+            updateLastCard(c);
+            return 1;
+        } else {
+            return -1; // indicate that the card cannot be played
+        }
+    }
+
+
+    /**
+     * Helper method for runTrunm, takes in user input and outputs 
+     * @return index of card
+     */
+    private int userInput(Hand currHand) {
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("Here is the top card in deck: " + _deck.getLastCard());
+        System.out.println("---------------------");
+
+        String prompt = "Please input which card you'd like to play (Input '1' if you want to play card 1):\n" + (currHand);
+        System.out.println(prompt);
+        int cardIndex = (sc.nextInt() - 1);  // Read user number input
+        // if () {} //checks to see if index is in range
+        return cardIndex;
+    }
+
+
+    /**
+     * The draw sequence when taking a card from the pile and putting it into a hand.
+     * Throws an error if the goWhere is invalid
+     * @param goWhere
+     */
+    public void draw(int turnNum) {
+        int whoseTurn = turnNum % 2; // 0 (when even, computer) or 1 (when odds, player) | 
+        int goWhere = whoseTurn+1; // have to subtract because in range of 1-2, but should be 0-1 (index range)
+        if ((whoseTurn < 0) || (whoseTurn >= (_hands.size()))) {
+            System.out.println("ERROR - CLASS MACAU, LINE 168 - index out of range");
+            return;
+        }
+        Card c = new Card();
+        c = _deck.draw(goWhere);
+        Hand currHand = _hands.get(whoseTurn);
+        currHand.add(c);
+    }
+
+
+    public void play(int turnNum, int cardIndex) {
+        int whoseTurn = turnNum % 2; // 0 (when even, computer) or 1 (when odds, player)
+        Hand currHand = _hands.get(whoseTurn);
+        Card c = currHand.play(cardIndex);
+        c.changeWhere(-1);
+        _deck.putInDiscard(c); // automatically updates last card var
+    }
+
+    /**
+     * updates last suit and last numb through deck class inside macau object.
+     * does not return anything
+     * @param c
+     */
+    public void updateLastCard(Card c) {
+        _deck.updateLastCard(c);
+    }
+
+    /**
+     * Increments _turnNumber var inside macau by one (+=1)
+     */
+    public void addTurn() {
+        _turnNumber+=1;
+    }
+
+    // ACCESSOR METHODS FOR DECK AND HANDS SO THAT CAN USE IN WOO
+
+    /**
+     * Accessor method for last card var in class deck inside macau object
+     * @return Card
+     */
+    public Card getLastCard() {
+        return _deck.getLastCard();
+    }
+
+<<<<<<< HEAD
+    public ArrayList<Card> getDiscard() {
+        return _deck.getDeck();
+      }
+    
+    public Deck getDeck() {
+    return _deck;
+    }
+=======
+    
+>>>>>>> b8902dab7a1dd011e8b0131630352d160f76374c
+
+
+    public static void main(String[] args) {
+        Macau m = new Macau();
+        while(m.gameOver() == -1) {
+            m.runTurn();
+            m._turnNumber += 1;
+            if(m.gameOver() != -1) {
+                break;
+            }
+        }
+        System.out.println("Player " + (m.gameOver() + 1) + " has won!");
+    }
+}
+
---
+++ v5.5/Woo.java
@@ -0,0 +1,134 @@
+public class Woo{
+
+
+  final static private String intro = "Welcome to Macau, brought to you by Blue Pizza Bagels Inc.\n"+
+  "Board Members: Andrew Piatetsky, Kosta Dubovskiy, Benjamin Belotser \n" +
+  "\n" +
+  "Rules:\n" + 
+  " • Players are given six cards from a standard 52 card deck. The first player to go can put down any card.\n" + 
+  " • After the first turn, the other player can put down a card that is either the same suit or number as the previous card played.\n" + 
+  " • There are four card numbers with special functions:\n" + 
+  "    • The 2 card forces the other player to draw 2 cards and skip their turn. \n" +
+  "    • The 4 card functions the same as two, except it forces the player to draw four instead of two cards. \n" + 
+  "    • The ace card forces the other player to skip a turn. \n" + 
+  "    • The jack card changes the suit of the deck to the suit of the jack played. \n" +
+  " • The game will end when either player runs out of cards in their hand. That player will be the winner. \n" +
+  " • You (The human player... I think) will always have the first turn \n" +
+  "\n" +
+  "Keybinds:\n" +
+  " • At the start of your turn, your hand will be shown to you, with your cards read in a line. \n" +
+  " • The card that you drew most recently will always be the leftmost card"
+  
+  
+  ;
+
+
+
+
+
+  public static void main(String[] args) {
+    System.out.println(intro);
+
+    Macau m = new Macau();
+    Card c = new Card();
+
+    for (int i = 0; i < 2; i++) {
+      // System.out.println(m.getHand(0));
+      // System.out.println("----------");
+      // System.out.println(m.getHand(1));
+      // System.out.println("----------");
+      // System.out.println(m._deck);
+      // System.out.println("----------");
+      // System.out.println((m._deck)._discard);
+      // System.out.println("-----------------------------------");
+      // System.out.println("-----------------------------------");
+
+
+      c = m.getDeck().draw(-1);
+      m.getDeck().putInDiscard(c);
+      m.updateLastCard(c);
+
+      // System.out.println("-----------------------------------");
+      // System.out.println("-----------------------------------");
+      // System.out.println(m.getHand(0));
+      // System.out.println("----------");
+      // System.out.println(m.getHand(1));
+      // System.out.println("----------");
+      // System.out.println(m._deck);
+      // System.out.println("----------");
+      System.out.println((m.getDeck()).getDiscard());
+      // System.out.println("----------");
+
+      System.out.println("\n");
+      System.out.println("-----------------------");
+      System.out.println(m.runTurn());
+
+      c = ((m.getDeck()).getDiscard()).get(((m.getDeck()).getDiscard()).size()-1);
+      m.updateLastCard(c);
+
+      m.addTurn();
+
+
+      System.out.println("-----------------------");
+      System.out.println("-------------------");
+      System.out.println((m.getDeck()).getDiscard());
+      System.out.println("----------");
+      System.out.println("-----------------------");
+      System.out.println(m.getHand(0));
+      System.out.println("----------");
+      System.out.println("-----------------------");
+      System.out.println(m.getHand(1));
+      System.out.println("-----------------------");
+      System.out.println("-----------------------");
+      System.out.println(m.getDeck() + "\n\n\n\n\n");
+    }
+
+
+
+
+
+
+
+
+
+    /*
+    // start of while loop
+    while (m.gameOver() == -1) { // checks to see if game is over
+
+      // Card q = new Card("Spades", 3);
+      // m.updateLastCard(q);
+      // System.out.println(m.getLastCard());
+
+      // // running turn
+      // int turnResult = m.runTurn();
+      // while (turnResult == -1) { // checks to see if card can be placed down
+      //   turnResult = m.runTurn();
+      // }
+
+      // // checking to see if special card
+      // Card c = new Card();
+      // if ( turnResult == 2) {
+      //   c = m.getLastCard();
+      //   String funct = c.getFunct();
+      //   if (funct == "+2") {
+      //     System.out.println("+2");
+      //   }
+      //   else if (funct == "+4") {
+      //     System.out.println("+4");
+      //   }
+      //   else if (funct == "Change Color") {
+      //     System.out.println("change color");
+      //   }
+      //   else if (funct == "Skip Turn") {
+      //     System.out.println("skip turn");
+      //   }
+      // } // end special function sorting
+
+      
+      // System.out.println((m._deck)._discard);
+
+
+      
+    } // end game while loop */
+  } // end main method
+} // end class woo
